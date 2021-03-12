93083b4fc4b7203db1b2e4f2660f488f902c1504
devhill535 <77030591+devhill535@users.noreply.github.com>
2021-03-12 22:59:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ client.on("ready", () => {
     `Online In Servers : ${client.guilds.size} | Users : ${client.users.size}`
   );
   let statuses = ['HELL is Here #0304',' /help ','MODIRATOR','GIF']
-    
-  
-   
- 
   ;
 
  setInterval(function() {
@@ -39,6 +35,31 @@ client.on("message", async message => {
     );
   }
 });
+
+///invite 
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  aliases: ["inv"],
+  description: "Invite the bot to your server.",
+  execute(message) {
+
+    let inviteEmbed = new MessageEmbed()
+      .setTitle("Add us to your server!")
+      .setDescription("Love using Harmony? Great, Thank you! Consider adding it to your server")
+      .setColor("#F0EAD6")
+      .setAuthor('Harmony','https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif')
+      .setThumbnail(message.guild.iconURL())
+      .addField(`Use the following link to add Harmony to your discord server`, 'https://discord.com/api/oauth2/authorize?client_id=733469607175913593&permissions=8&scope=bot', true)
+
+    inviteEmbed.setTimestamp();
+
+    return message.channel.send(inviteEmbed).catch(console.error);
+  }
+};
+
+
 
 client.on("message", message => {
   var command = message.content;
