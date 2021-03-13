7a25c5214348e86c63b48d3c58928ad5dbb22c6a
devhill535 <77030591+devhill535@users.noreply.github.com>
2021-03-13 17:11:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -388,7 +388,7 @@ client.on("message", message => {
 });
 
 client.on("message", message => {
-  if (message.content.startsWith(prefix + "/man")) {
+  if (message.content.startsWith(prefix + "/man" , "Man")) {
     
     exports.conf = {
 
