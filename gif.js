5b8974df4f60490f1b79b8828b99d54ac28ab715
devhill535 <77030591+devhill535@users.noreply.github.com>
2021-03-14 19:47:29 +0300

    Create gif.js

---
+++ gif.js
@@ -0,0 +1,20 @@
+client.on("message", message => {
+  if (message.content === prefix + "/gif") {
+    var addserver = `https://discord.gg/n8kXNUG2jn`;
+    var SUPPORT = `https://discord.gg/9jyZWX4Fmc`;
+    let embed = new Discord.RichEmbed()
+      .setAuthor(message.author.username)
+      .setThumbnail(message.author.avatarURL)
+      .setFooter("")
+      .setImage(
+        "https://cdn.discordapp.com/attachments/796750980469358622/809873763444850698/image0.gif"
+      )
+      .setColor("RANDOM").setDescription(` 
+
+
+
+
+
+
+
+
