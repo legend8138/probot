f2a137446dd4d1555780d6a16a25f71d75f0f61c
devhill535 <77030591+devhill535@users.noreply.github.com>
2021-03-14 20:08:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1084,31 +1084,6 @@ client.on("message", message => {
 });
 
 
-
-client.on("message", message => {
-  if (message.content === prefix + "/gif") {
-    var addserver = `https://discord.gg/n8kXNUG2jn`;
-    var SUPPORT = `https://discord.gg/9jyZWX4Fmc`;
-    let embed = new Discord.RichEmbed()
-      .setAuthor(message.author.username)
-      .setThumbnail(message.author.avatarURL)
-      .setFooter("")
-      .setImage(
-        "https://cdn.discordapp.com/attachments/796750980469358622/809873763444850698/image0.gif"
-      )
-      .setColor("RANDOM").setDescription(` 
- /meme,/boomb,/man gif,couple,girl gif,sad gif,
-
-
-message.channel.sendEmbed(embed);
-  }
-});
-
-
-
-
-
-
 client.on("message", message => {
   if (message.content.startsWith(prefix + "/sad gif")) {
     let sads = [
