f7524b8beaaf945e8c7642edbceb30e8c8f5bc57
devhill535 <77030591+devhill535@users.noreply.github.com>
2021-03-15 18:40:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -567,36 +567,6 @@ client.on("message", message => {
 });
 
 
-client.on("message", message => {
-  if (message.content.startsWith(prefix + "/mr robot")) {
-    let loves = [
-"https://media.giphy.com/media/dLolp8dtrYCJi/giphy.gif",
-     "https://media.giphy.com/media/3oEjHWpiVIOGXT5l9m/giphy.gif",
-      "https://media.giphy.com/media/26BRM3JGiFlhOQkmY/giphy.gif",
-      "https://media.giphy.com/media/JFo3BEQFmK4cU/giphy.gif",
-      "https://media.giphy.com/media/3oEjI7gBSMd2jtZOI8/giphy.gif",
-      "https://media.giphy.com/media/rKydlqfyxtjRS/giphy.gif",
-      "https://media.giphy.com/media/J8t9H4Z7Wp3fq/giphy.gif",
-      "https://media.giphy.com/media/AiDbw1OSisyoE/giphy.gif",
-      "https://media.giphy.com/media/xTiTntsBQI0Zmu9YoE/giphy.gif",
-
-    ];
-
-    message.channel
-      .send({
-        embed: {
-          description: `${message.author.username} MR ROBOT GIF  `,
-          image: {
-            url: loves[Math.floor(Math.random() * loves.length)]
-          }
-        }
-      })
-      .catch(e => {
-        client.log.error(e);
-      });
-  }
-});
-
 
 client.on("message", message => {
   if (message.content.startsWith(prefix + "/anime")) {
